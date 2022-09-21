b288f863b0fce15abc6c0364745b32d762e9a54a
Sascha Geprägs <gepraegssascha@gmail.com>
2022-09-21 18:41:15 +0200

    silbermine-intro

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var goldmine_level = 0;
 var geld = 100;
 var goldmine_faktor = 0;
 var diamine_level = 0;
+var silbermine_level = 0;
 
 if (localStorage.getItem("gameIsSaved") == null) {
     localStorage.setItem("gameIsSaved", true);
@@ -23,7 +24,10 @@ function update() {
     document.getElementById("diamine-levelangabe").innerHTML = "Level: " + diamine_level + "/10";
     document.getElementById("diamine_geldsekunde").innerHTML = diamine_level * 3 + " Geld/Sekunde";
     document.getElementById("diamine_progress").value = diamine_level;
-    document.getElementById("gpros").innerHTML = "Aktuell machst du " + allg_faktor + " Geld pro Sekunde"
+    document.getElementById("gpros").innerHTML = "Aktuell machst du " + allg_faktor + " Geld pro Sekunde";
+    document.getElementById("silbermine-levelangabe").innerHTML = "Level: " + silbermine_level + "/10";
+    document.getElementById("silbermine_geldsekunde").innerHTML = silbermine_level / 3 + " Geld/Sekunde";
+    document.getElementById("silbermine_progress").value = silbermine_level;
 
 }
 
@@ -31,6 +35,7 @@ function gameSave() {
     localStorage.setItem("goldmine_level", goldmine_level);
     localStorage.setItem("geld", geld);
     localStorage.setItem("diamine_level", diamine_level);
+    localStorage.setItem("silbermine_level", silbermine_level);
     update();
 }
 
@@ -38,6 +43,7 @@ function gameLoad() {
     goldmine_level = localStorage.getItem("goldmine_level");
     geld = localStorage.getItem("geld");
     diamine_level = localStorage.getItem("diamine_level");
+    silbermine_level = localStorage.getItem("silbermine_level");
     update();
 }
 
@@ -59,9 +65,23 @@ function diamine_upgrade() {
     if (diamine_level > 9) {
         window.alert("Du hast bereits das Maximum erreicht!");
     } else {
-        if (geld > 100 || geld == 100) {
+        if (geld > 1000 || geld == 1000) {
             geld = geld - 1000;
             diamine_level++;
+            gameSave();
+        } else {
+            window.alert("Du hast nicht genügend Geld für diesen Kaufvorgang!")
+        }
+    }
+}
+
+function silbermine_upgrade() {
+    if (silbermine_level > 9) {
+        window.alert("Du hast bereits das Maximum erreicht!");
+    } else {
+        if (geld > 10 || geld == 10) {
+            geld = geld - 10;
+            silbermine_level++;
             gameSave();
         } else {
             window.alert("Du hast nicht genügend Geld für diesen Kaufvorgang!")
@@ -77,6 +97,12 @@ var goldmine_geldmachen = window.setInterval(function() {
 
 var diamine_geldmachen = window.setInterval(function() {
     geld = Number(geld) + Number((3 * diamine_level));
+    update();
+    gameSave();
+}, 1000);
+
+var silbermine_geldmachen = window.setInterval(function() {
+    geld = Number(geld) + Number((3 / silbermine_level));
     update();
     gameSave();
 }, 1000);
