adeda6ac2f60bf6eac508a2acdc5f1c4917e09f4
Sascha Geprägs <gepraegssascha@gmail.com>
2022-09-21 16:42:06 +0200

    diamine-intro

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var goldmine_level = 0;
 var geld = 100;
 var goldmine_faktor = 0;
+var goldmine_level = 0;
 
 if (localStorage.getItem("gameIsSaved") == null) {
     localStorage.setItem("gameIsSaved", true);
@@ -19,17 +20,23 @@ function update() {
     document.getElementById("Kontostand").innerHTML = "Kontostand: " + geld;
     document.getElementById("goldmine_geldsekunde").innerHTML = goldmine_level + " Geld/Sekunde";
     document.getElementById("goldmine_progress").value = goldmine_level;
+    document.getElementById("diamine-levelangabe").innerHTML = "Level: " + diamine_level + "/10";
+    document.getElementById("diamine_geldsekunde").innerHTML = diamine_level + " Geld/Sekunde";
+    document.getElementById("diamine_progress").value = diamine_level;
+
 }
 
 function gameSave() {
     localStorage.setItem("goldmine_level", goldmine_level);
     localStorage.setItem("geld", geld);
+    localStorage.setItem("diamine_level", diamine_level);
     update();
 }
 
 function gameLoad() {
     goldmine_level = localStorage.getItem("goldmine_level");
     geld = localStorage.getItem("geld");
+    goldmine_level = localStorage.getItem("diamine_level");
     update();
 }
 
@@ -47,8 +54,28 @@ function goldmine_upgrade() {
     }
 }
 
+function diamine_upgrade() {
+    if (diamine_level > 9) {
+        window.alert("Du hast bereits das Maximum erreicht!");
+    } else {
+        if (geld > 100 || geld == 100) {
+            geld = geld - 100;
+            diamine_level++;
+            gameSave();
+        } else {
+            window.alert("Du hast nicht genügend Geld für diesen Kaufvorgang!")
+        }
+    }
+}
+
 var goldmine_geldmachen = window.setInterval(function() {
     geld = Number(geld) + Number((5 * goldmine_level));
+    update();
+    gameSave();
+}, 5000);
+
+var diamine_geldmachen = window.setInterval(function() {
+    geld = Number(geld) + Number((5 * diamine_level));
     update();
     gameSave();
 }, 5000);
